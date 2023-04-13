9255f9d76e140675daf1256b130f1e523a72f34e
TheBlackFern <doroginin.dv@phystech,edu>
2023-04-13 19:26:41 +0300

    :construction: implemented models, added test examples to db Cards are separated into groups, both groups and cards have their respective tables.

--- src/carddeck/models.py
+++ src/carddeck/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 
+class CardGroup(models.Model):
+    name = models.CharField(max_length=100)
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.name
 
+class Card(models.Model):
+    word = models.CharField(max_length=100)
+    group = models.ForeignKey(CardGroup, on_delete=models.CASCADE)
+    explanation = models.CharField(max_length=100)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    
-
-class CardText(models.Model):
-    top_text = models.CharField(max_length=100)
-    bottom_text = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.word}: {self.explanation}"
--- src/carddeck_website/settings.py
+++ src/carddeck_website/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "carddeck",
+    "carddeck.apps.CarddeckConfig",
 ]
 
 MIDDLEWARE = [
