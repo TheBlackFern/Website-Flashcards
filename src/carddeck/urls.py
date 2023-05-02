7fa0c25e606ad1cb6b19d8f796d889a7b3065c85
TheBlackFern <doroginin.dv@phystech,edu>
2023-05-02 19:46:31 +0300

    🚀 fully functional website 💄 added a home page and a nav bar ✨ test for groups individually ✨ validationg before adding a new group

--- src/carddeck/urls.py
+++ src/carddeck/urls.py
@@ -4,13 +4,15 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("cards/", views.cards, name="cards"),
     path("new_card/", views.add_card, name="card_added"),
     path("update_card/", views.update_card, name="card_updated"),
     path("increment-card/<int:card_id>/", views.increment_card, name="card_incremented"),
     path("decrement-card/<int:card_id>/", views.decrement_card, name="card_decremented"),
     path("delete-card/<int:card_id>/", views.delete_card, name="card_deleted"),
+    path("test", views.test, name="test"),
+    path("test/<int:group_id>", views.test_one, name="test_one"),
     path("groups/", views.show_groups, name="groups"),
-    path("groups/test", views.test, name="test"),
-    path("groups/new_group/", views.group_added, name="group_added"),
+    path("new_group/", views.check_and_add_group),
     path("delete-group/<int:group_id>/", views.delete_group, name="group_deleted"),
 ]
--- src/carddeck/views.py
+++ src/carddeck/views.py
@@ -1,21 +1,61 @@
 import random
 import json
+from itertools import accumulate
 
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from django.views.decorators.csrf import csrf_exempt
+from django.db.utils import IntegrityError
 from .models import CardGroup, Card
 
 def index(request):
+    cards = Card.objects.all()
+    amounts = [0] * 4
+    for card in cards:
+        if card.level <= 2:
+            amounts[0] += 1
+        elif 2 < card.level <= 4:
+            amounts[1] += 1
+        elif 4 < card.level <= 8:
+            amounts[2] += 1
+        else:
+            amounts[3] += 1
+    percentages = list(map(lambda x: x / len(cards), amounts))
+    sum_percs = list(accumulate(map(lambda x: x / len(cards), amounts)))
+    for_piechart = [str(int(round(n*100, 0)))+"%" for n in sum_percs]
+    context = {
+        "percantages_1": for_piechart[0],
+        "percantages_2": for_piechart[1],
+        "percantages_3": for_piechart[2],
+        "perc1": str(int(round(100 * percentages[0], 0))) + "%",
+        "perc2": str(int(round(100 * percentages[1], 0))) + "%",
+        "perc3": str(int(round(100 * percentages[2], 0))) + "%",
+        "perc4": str(int(round(100 * percentages[3], 0))) + "%",
+    }
+    return render(request, "pages/home.html", context)
+
+def cards(request):
     groups = CardGroup.objects.all()
     groups_cards = [(group, reversed(group.card_set.all())) for group in groups]
-    return render(request, "pages/main.html", {"groups_cards": groups_cards})
+    return render(request, "pages/cards.html", {"groups_cards": groups_cards})
 
 def test(request):
     rands = [bool(random.getrandbits(1)) for _ in range(Card.objects.count())]
     cards_rand = [(model_to_dict(card), rand) for card, rand 
                   in zip(Card.objects.order_by("?"), rands)]
+    js_cards_rands = json.dumps(cards_rand)
+    context = {
+        "cards_rand": cards_rand,
+        "js_cards_rands": js_cards_rands,
+    }
+    return render(request, "pages/test.html", context)
+
+def test_one(request, group_id):
+    group = CardGroup.objects.get(id=group_id)
+    rands = [bool(random.getrandbits(1)) for _ in range(group.card_set.all().count())]
+    cards_rand = [(model_to_dict(card), rand) for card, rand 
+                  in zip(group.card_set.all().order_by("?"), rands)]
     js_cards_rands = json.dumps(cards_rand)
     context = {
         "cards_rand": cards_rand,
@@ -35,11 +75,17 @@ def show_groups(request):
     }
     return render(request, "pages/groups.html", context)
 
-def group_added(request):
-    group_name = request.GET["group_name"]
-    context = {"group_name": group_name}
-    CardGroup.objects.create(name=group_name)
-    return render(request, "pages/new_group.html", context)
+
+@csrf_exempt
+def check_and_add_group(request):
+    group_name = request.POST.get("group_name")
+    if (not group_name):
+        return JsonResponse({"valid": False})
+    try:
+        CardGroup.objects.create(name=group_name)
+        return JsonResponse({"valid": True})
+    except IntegrityError:
+        return JsonResponse({"valid": False})
 
 @csrf_exempt
 def add_card(request):
